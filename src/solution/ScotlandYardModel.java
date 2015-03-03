1741ac4c3a13b8b94fd3dabc9b27df90af33261b
Chris Leach <chris_q@hotmail.co.uk>
2015-03-03 02:39:20 +0000

    Formatted to Allman style, implemented basic constructor for ScotlandYardModel (SYM)

--- src/solution/ScotlandYardModel.java
+++ src/solution/ScotlandYardModel.java
@@ -7,95 +7,123 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public class ScotlandYardModel extends ScotlandYard {
-
-
-    public ScotlandYardModel(int numberOfDetectives, List<Boolean> rounds, String graphFileName) throws IOException {
+public class ScotlandYardModel extends ScotlandYard
+{
+    int numberOfDetectives;
+    List<Boolean> rounds;
+    
+    Graph<Integer,Route> graph;
+    
+    public ScotlandYardModel(int numberOfDetectives, List<Boolean> rounds, String graphFileName) throws IOException
+    {
         super(numberOfDetectives, rounds, graphFileName);
+        
+        this.numberOfDetectives = numberOfDetectives;
+        this.rounds = rounds;
+        
+        GraphReader r = new ScotlandYardGraphReader();
+        this.graph = r.readGraph(graphFileName);
     }
-
+    
     @Override
-    protected Move getPlayerMove(Colour colour) {
+    protected Move getPlayerMove(Colour colour)
+    {
         return null;
     }
-
+    
     @Override
-    protected void nextPlayer() {
-
+    protected void nextPlayer()
+    {
+        
     }
-
+    
     @Override
-    protected void play(MoveTicket move) {
-
+    protected void play(MoveTicket move)
+    {
+        
     }
-
+    
     @Override
-    protected void play(MoveDouble move) {
-
+    protected void play(MoveDouble move)
+    {
+        
     }
-
+    
     @Override
-    protected void play(MovePass move) {
-
+    protected void play(MovePass move)
+    {
+        
     }
-
+    
     @Override
-    protected List<Move> validMoves(Colour player) {
+    protected List<Move> validMoves(Colour player)
+    {
         return null;
     }
-
+    
     @Override
-    public void spectate(Spectator spectator) {
-
+    public void spectate(Spectator spectator)
+    {
+        
     }
-
+    
     @Override
-    public boolean join(Player player, Colour colour, int location, Map<Ticket, Integer> tickets) {
+    public boolean join(Player player, Colour colour, int location, Map<Ticket, Integer> tickets)
+    {
         return false;
     }
-
+    
     @Override
-    public List<Colour> getPlayers() {
+    public List<Colour> getPlayers()
+    {
         return null;
     }
-
+    
     @Override
-    public Set<Colour> getWinningPlayers() {
+    public Set<Colour> getWinningPlayers()
+    {
         return null;
     }
-
+    
     @Override
-    public int getPlayerLocation(Colour colour) {
+    public int getPlayerLocation(Colour colour)
+    {
         return 0;
     }
-
+    
     @Override
-    public int getPlayerTickets(Colour colour, Ticket ticket) {
+    public int getPlayerTickets(Colour colour, Ticket ticket)
+    {
         return 0;
     }
-
+    
     @Override
-    public boolean isGameOver() {
+    public boolean isGameOver()
+    {
         return false;
     }
-
+    
     @Override
-    public boolean isReady() {
+    public boolean isReady()
+    {
         return false;
     }
-
+    
     @Override
-    public Colour getCurrentPlayer() {
+    public Colour getCurrentPlayer()
+    {
         return null;
     }
-
+    
     @Override
-    public int getRound() {
+    public int getRound()
+    {
         return 0;
     }
-
+    
     @Override
-    public List<Boolean> getRounds() {
+    public List<Boolean> getRounds()
+    {
         return null;
     }
 }
