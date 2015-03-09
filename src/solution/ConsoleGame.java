c35060da94785cf259c30b17482e929b78e4f73f
Chris Leach <chris_q@hotmail.co.uk>
2015-03-09 09:05:57 +0000

    lib/ and resources/ added to repo. svgSalamander library added. build.xml changed to add svgSalamander to classpath.

---
+++ src/solution/ConsoleGame.java
@@ -0,0 +1,12 @@
+package solution;
+
+import com.kitfox.svg.*;
+
+// Testing only for now.
+class ConsoleGame
+{
+    public static void main(String[] args)
+    {
+        SVGUniverse u = new SVGUniverse();
+    }
+}
