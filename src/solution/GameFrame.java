26284d8800a407c955a0147884978c91a25519d6
Chris Leach <chris_q@hotmail.co.uk>
2015-03-13 12:21:42 +0000

    Implemented MenuPanel graphics with placeholder SVG.

--- src/solution/GameFrame.java
+++ src/solution/GameFrame.java
@@ -2,6 +2,8 @@ package solution;
 
 import scotlandyard.*;
 
+import com.kitfox.svg.*;
+
 import javax.swing.*;
 
 public class GameFrame extends JFrame
@@ -10,19 +12,25 @@ public class GameFrame extends JFrame
     JScrollPane view;
     MenuPanel menu;
     
+    SVGUniverse universe;
+    
     public GameFrame()
     {
         game = new GamePanel();
         view = new JScrollPane();
         menu = new MenuPanel();
         
-        view.add(game);
+        universe = SVGCache.getSVGUniverse();
         
+        this.setTitle("Scotland Yard");
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        this.setLocationByPlatform(true);
+        this.setSize(800, 600);
+        
+        game.setPreferredSize(getSize());
+        
+        view.add(game);
         this.add(menu);
         pack();
-        
-        setTitle("Scotland Yard");
-        setDefaultCloseOperation(this.EXIT_ON_CLOSE);
-        setLocationByPlatform(true);
     }
 }
--- src/solution/MenuPanel.java
+++ src/solution/MenuPanel.java
@@ -4,12 +4,28 @@ import scotlandyard.*;
 
 import com.kitfox.svg.*;
 
+import java.io.*;
+import java.net.*;
 import javax.swing.*;
 
-public class MenuPanel extends JPanel
+public class MenuPanel extends SVGDisplayPanel
 {
+    SVGUniverse uni;
+    
     public MenuPanel()
     {
-        this.add(new JLabel("Scotland Yard!"));
+        uni = SVGCache.getSVGUniverse();
+        
+        URL menuURL = null;
+        URI menuURI = null;
+        
+        try
+        {
+            menuURL = new File("resources/dist/menu.svg").toURI().toURL();
+            menuURI = uni.loadSVG(menuURL);
+        }
+        catch (Exception e) { }
+        
+        this.setDiagram(uni.getDiagram(menuURI));
     }
 }
