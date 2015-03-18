89c11e89ea34ca8cf583bb12a4da440f44640ff2
Chris Leach <chris_q@hotmail.co.uk>
2015-03-18 10:17:04 +0000

    Refactored PlayerSetupPanel. Removed 'Computer' option.

--- src/solution/PlayerSetupPanel.java
+++ src/solution/PlayerSetupPanel.java
@@ -3,7 +3,6 @@ package solution;
 import scotlandyard.*;
 
 import javax.swing.*;
-import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -11,76 +10,54 @@ public class PlayerSetupPanel extends JPanel implements ActionListener
 {
     public enum PlayerType
     {
-        Player, Computer, Off
+        Player, Off
     }
     
-    String name;
-    String file;
-    Colour colour;
-    PlayerType pt;
+    private Colour colour;
+    private String name;
     
-    ImageIcon imgIcon;
-    ImageIcon nullIcon;
-    JLabel imgLabel;
-    JComboBox<String> ptBox;
+    private ImageIcon imgIcon;
+    private static ImageIcon nullIcon = new ImageIcon("resources/dist/null-lo.png");
+    private JLabel imgLabel;
+    
+    private PlayerType pt;
+    private JComboBox<String> ptBox;
     
     public PlayerSetupPanel(int i)
     {
-        nullIcon = new ImageIcon("resources/dist/null-lo.png");
-        switch (i)
+        colour = Colour.values()[i];
+        if (i == 0)
         {
-            case 0:
-                colour = Colour.Black;
-                name = "Mr. X";
-                imgIcon = new ImageIcon("resources/dist/mr-x-lo.png");
-                break;
-            case 1:
-                colour = Colour.Blue;
-                name = "Detective 1";
-                imgIcon = new ImageIcon("resources/dist/det-b-lo.png");
-                break;
-            case 2:
-                colour = Colour.Green;
-                name = "Detective 2";
-                imgIcon = new ImageIcon("resources/dist/det-g-lo.png");
-                break;
-            case 3:
-                colour = Colour.Red;
-                name = "Detective 3";
-                imgIcon = new ImageIcon("resources/dist/det-r-lo.png");
-                break;
-            case 4:
-                colour = Colour.White;
-                name = "Detective 4";
-                imgIcon = new ImageIcon("resources/dist/det-w-lo.png");
-                break;
-            case 5:
-                colour = Colour.Yellow;
-                name = "Detective 5";
-                imgIcon = new ImageIcon("resources/dist/det-y-lo.png");
-                break;
+            name = "Mr. X";
+            imgIcon = new ImageIcon("resources/dist/mr-x-lo.png");
+        }
+        else
+        {
+            name = "Detective " + i;
+            String[] cs = {"b","g","r","w","y",};
+            imgIcon = new ImageIcon("resources/dist/det-" + cs[i-1] + "-lo.png");
         }
         
         pt = PlayerType.Player;
-        String[] options;
-        if (i == 0) options = new String[] {"Player", "Computer"};
-        else        options = new String[] {"Player", "Computer", "Off"};
-        ptBox = new JComboBox<String>(options);
+        if (i == 0) ptBox = new JComboBox<String>(new String[] {"Player"});
+        else        ptBox = new JComboBox<String>(new String[] {"Player", "Off"});
         ptBox.addActionListener(this);
         
         this.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         
+        // Set out name label.
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.insets = new Insets(0, 0, 0, 0);
         this.add(new JLabel(name), gbc);
         
+        // Set out image.
         gbc.gridy = 1;
         gbc.insets = new Insets(10, 0, 5, 0);
         imgLabel = new JLabel(imgIcon);
         this.add(imgLabel, gbc);
         
+        // Set out combo box.
         gbc.gridy = 2;
         gbc.insets = new Insets(0, 0, 0, 0);
         this.add(ptBox, gbc);
@@ -88,26 +65,26 @@ public class PlayerSetupPanel extends JPanel implements ActionListener
     
     public void actionPerformed(ActionEvent e)
     {
-        switch (ptBox.getItemAt(ptBox.getSelectedIndex()))
+        if (ptBox.getItemAt(ptBox.getSelectedIndex()) == "Player")
         {
-            case "Player":
-                pt = PlayerType.Player;
-                imgLabel.setIcon(imgIcon);
-                break;
-            case "Computer":
-                pt = PlayerType.Computer;
-                imgLabel.setIcon(imgIcon);
-                break;
-            default:
-                pt = PlayerType.Off;
-                imgLabel.setIcon(nullIcon);
+            pt = PlayerType.Player;
+            imgLabel.setIcon(imgIcon);
+        }
+        else
+        {
+            pt = PlayerType.Off;
+            imgLabel.setIcon(nullIcon);
         }
         imgLabel.revalidate();
-        System.err.println(name + " player type changed to: " + ptBox.getSelectedIndex());
     }
     
     public PlayerType getPlayerType()
     {
         return pt;
+    }
+    
+    public Colour getColour()
+    {
+        return colour;
     }
 }
